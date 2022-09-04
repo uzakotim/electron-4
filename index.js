8a68a78be8e71370c049f63d10bd1ad9900e6ed1
uzakotim <timauzakov@gmail.com>
2022-09-04 19:36:35 +0200

    hybrid app

---
+++ index.js
@@ -0,0 +1,9 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindows()
+{
+    let appWindow = new BrowserWindow();
+    appWindow.loadFile('./index.html');
+}
+
+app.on("ready",createWindows);
